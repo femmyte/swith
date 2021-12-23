69ffb3713670ecd88fb5b76ab4b3eecf0033abbd
Akintola Oluwafemi <akintolaoluwafemi@Abisades-MBP.mshome.net>
2021-12-23 18:43:23 +0100

    First Commit

---
+++ swith/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="card">
+        
+        <div className="card-title text-primary mt-4">SWITH Global Market Hub</div>
+        <p className="card-body"><b>Project Topic: </b>A startup that want to make it easier for companies to purchase goods from China.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
