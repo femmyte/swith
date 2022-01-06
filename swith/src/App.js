be103d48268f1d6a81f1bb53a88f78bf04cfafdc
Akintola Oluwafemi <akintolaoluwafemi@Abisades-MacBook-Pro.local>
2022-01-06 22:06:51 +0100

    Adding Componenets

--- swith/src/App.js
+++ swith/src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Header from "./components/Header"
+import SectionOne from "./components/SectionOne"
+import SectionTwo from "./components/SectionTwo"
+import SectionThree from "./components/SectionThree"
+import Footer from "./components/Footer"
+import Login from "./components/Login"
 
 function App() {
   return (
+
     <div className="App">
+      {/* 
+      <Header/>
+      <SectionOne/>
+      <SectionTwo/>
+      <SectionThree/>
+      <Footer/>
+      <Login/> */}
+
       <header className="card">
         
         <div className="card-title text-primary mt-4">SWITH Global Market Hub</div>
